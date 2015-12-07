0605bae43a94c1f353ea9f1d188a8c11629745a1
Amanda M <amarrione@gmail.com>
2015-12-07 14:33:56 -0500

    Changed driver setup/config

--- src/main/java/utilities/WebDriverManager.java
+++ src/main/java/utilities/WebDriverManager.java
@@ -1,18 +1,9 @@
 package utilities;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class WebDriverManager extends ReadPropFile{
+public class WebDriverManager {
 	
 	/** WebDriver instance. */
 	private WebDriver driver;
@@ -44,7 +35,7 @@ public class WebDriverManager extends ReadPropFile{
 	 */
 	public WebDriver driver() {
 		if (driver == null) {
-			driver = createDriver();
+			driver = WebDriverFactory.createDriver();
 		}
 		return driver;
 	}
@@ -54,43 +45,9 @@ public class WebDriverManager extends ReadPropFile{
 	 * @return a new webdriver instance
 	 */
 	public WebDriver newDriver() {
-		createDriver();
+		WebDriverFactory.createDriver();
 		return driver;
 	}
-	public WebDriver createDriver() {
-		try {
-			checkIfRemote();
-		} catch (IOException e1) {
-			throw new IllegalStateException("settings.properties file did not have isRemote field");
-		}
-		
-		DesiredCapabilities capability = new DesiredCapabilities();
-		capability.setPlatform(Platform.ANY);
-		capability.setJavascriptEnabled(true);
-		capability = DesiredCapabilities.firefox();
-		
-		if (isRemote) {
-			RemoteWebDriver driver = new FirefoxDriver(capability);
-			
-			try {
-			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-			
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-			return driver;
-		}
-		else {
-			WebDriver driver = new FirefoxDriver(capability);
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-			return driver;
-		}
-	}
-	
 
 	/***********************************/
 	/** Typical singleton boilerplate **/
